4445e30a33fcf0429a94f06b7b69b17c39617681
Alexander Fenster <fenster@fenster.name>
2015-08-04 04:16:57 +0300

    formatting

--- index.pl
+++ index.pl
@@ -11,66 +11,9 @@ use db;
 
 use CGI::Pretty qw(:standard);
 
-print header(-type => 'text/html', -charset => 'utf-8');
-print start_html(-title => "Unconscious bias");
-print h1("Unconscious bias");
-
-print "<div id=\"preface\">\n";
-
-my $discussions = 
-print p(<<EOD);
-The idea of this test is to show you that it can be very difficult to find out if a piece of code is written by a male or a female programmer.
-I had been teaching first year students in Novosibirsk (Russia) for a few years, and I have a database containing all their solutions to 
-simple tasks, such as basic algorithms, searching, sorting, and graphs; most of them are written in C, some in C++.
-That's a big amount of data that can be used for making funny things like this one.
-EOD
-
-print p(<<EOD);
-Interested? Let's start then.
-EOD
-
-print a({-href => 'http://feldgendler.livejournal.com/155015.html'}, 'Here'), " is a link to the original discussion, in Russian; 
-thanks to Alexey Feldgendler for hosting that discussion and inspiring me to make this test.";
-
-print h2("Rules");
-
 my $total = db::first_field("SELECT COUNT(1) FROM bias");
 my $male = db::first_field("SELECT COUNT(1) FROM bias WHERE gender = 'm'");
 my $female = db::first_field("SELECT COUNT(1) FROM bias WHERE gender = 'f'");
-print p(<<EOD);
-You will be given ten code fragments, some of them were written by female students, others by male students.
-For most of them it was the first year of computer programming and the first year of learning C programming language.
-EOD
-
-print p(<<EOD);
-These fragments are picked randomly from a database which has $total programs, $male of them written by male students and 
-$female by female students, but each of the fragments shown to you can be written by either male or female student with an equal probability.
-EOD
-
-print p(<<EOD);
-Try to guess who is the author of each fragment.
-If you refresh the page new programs will be shown and the test will restart (you will lose your progress). 
-EOD
-
-print p(small(<<EOD));
-Note: unfortunately, due to a series of database backup-restore operations, all non-ASCII characters in the code fragments were lost and are shown as question marks.
-But that's good for our test as it makes the task even harder as sometimes you could guess the author by reading his or her comments in Russian.
-EOD
-
-print <<EOD;
-</div>
-<div id="start_button">
-<button onclick=\"next()\">Start test</button>
-</div>
-
-<div id="final" style="display: none">
-<span style="font-size: 24px"><b>Results</b></span><br/><br/>
-Congratulations! You've got <b><span id="final_correct"></span> correct out of <span id="final_total"></span></b>.<br/><br/>
-You can try to pass this test again if you refresh the page.
-</div>
-
-EOD
-
 my @male = db::query_hashref("SELECT * FROM bias WHERE gender = 'm' ORDER BY rand() LIMIT 20");
 my @female = db::query_hashref("SELECT * FROM bias WHERE gender = 'f' ORDER BY rand() LIMIT 20");
 my @random = shuffle(@male, @female);
@@ -94,7 +37,63 @@ foreach my $code (@random)
 }
 my $jsondump = to_json($json);
 
+
+
+print header(-type => 'text/html', -charset => 'utf-8');
+print start_html(-title => "Unconscious bias");
+print h1("Unconscious bias");
+
 print <<EOD;
+<div id="preface">
+
+<p>
+The idea of this test is to show you that it can be very difficult to find out if a piece of code is written by a male or a female programmer.
+I had been teaching first year students in Novosibirsk (Russia) for a few years, and I have a database containing all their solutions to 
+simple tasks, such as basic algorithms, searching, sorting, and graphs; most of them are written in C, some in C++.
+That's a big amount of data that can be used for making funny things like this one.
+</p>
+
+<p>
+Interested? Let's start then.
+</p>
+
+<p>
+<a href="http://feldgendler.livejournal.com/155015.html">Here</a> is a link to the original discussion, in Russian; 
+thanks to Alexey Feldgendler for hosting that discussion and inspiring me to make this test.
+</p>
+
+<h2>Rules</h2>
+
+<p>
+You will be given ten code fragments, some of them were written by female students, others by male students.
+For most of them it was the first year of computer programming and the first year of learning C programming language.
+</p>
+
+<p>
+These fragments are picked randomly from a database which has $total programs, $male of them written by male students and 
+$female by female students, but each of the fragments shown to you can be written by either male or female student with an equal probability.
+</p>
+
+<p>
+Try to guess who is the author of each fragment.
+If you refresh the page new programs will be shown and the test will restart (you will lose your progress). 
+</p>
+
+<p><small>
+Note: unfortunately, due to a series of database backup-restore operations, all non-ASCII characters in the code fragments were lost and are shown as question marks.
+But that's good for our test as it makes the task even harder as sometimes you could guess the author by reading his or her comments in Russian.
+</small></p>
+</div>
+<div id="start_button">
+<button onclick=\"next()\">Start test</button>
+</div>
+
+<div id="final" style="display: none">
+<span style="font-size: 24px"><b>Results</b></span><br/><br/>
+Congratulations! You've got <b><span id="final_correct"></span> correct out of <span id="final_total"></span></b>.<br/><br/>
+You can try to pass this test again if you refresh the page.
+</div>
+
 <div id="code" style="display: none">
 Code fragment #<span id="number"></span>. Correct answers so far: <span id="correct_answers"></span> of <span id="total"></span>.
 <hr/>
@@ -189,11 +188,9 @@ function selected(gender)
 }
 
 </script>
-EOD
 
-print "Alexander Fenster, fenster<span id=\"at\"></span>fenster<span id=\"dot\"></span>name, ", a({-href => 'https://plus.google.com/+AlexanderFenster'}, 'https://plus.google.com/+AlexanderFenster'), '.';
+Alexander Fenster, fenster<span id="at"></span>fenster<span id="dot"></span>name, <a href="https://plus.google.com/+AlexanderFenster">https://plus.google.com/+AlexanderFenster</a>.
 
-print <<EOD;
 <script>
 document.getElementById('at').innerHTML = '\@';
 document.getElementById('dot').innerHTML = '.';
